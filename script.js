f4e79a9c080f3667662fce5f0c4effeb80a14364
kimnerd <hyuntaec@naver.com>
2025-08-02 00:06:28 +0100

    Update puzzle size handling

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const boardElem = document.getElementById("board");
-const boardSize = 6;
+const sizeSelect = document.getElementById("sizeSelect");
+let boardSize = parseInt(sizeSelect.value, 10);
+let currentDifficulty = "easy";
 
-function generatePuzzle(difficulty) {
-  const puzzle = PUZZLES[difficulty];
+sizeSelect.addEventListener("change", () => {
+  generatePuzzle();
+});
+
+function emptyBoard(size) {
+  const board = [];
+  for (let r = 0; r < size; r++) {
+    board.push(Array(size).fill(""));
+  }
+  return board;
+}
+
+function generatePuzzle(difficulty = currentDifficulty) {
+  currentDifficulty = difficulty;
+  boardSize = parseInt(sizeSelect.value, 10);
+  let puzzle = PUZZLES[currentDifficulty];
+  if (!puzzle || puzzle.length !== boardSize) {
+    puzzle = emptyBoard(boardSize);
+  }
   boardElem.innerHTML = "";
-  boardElem.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`;
+  boardElem.style.gridTemplateColumns = `repeat(${boardSize}, 48px)`;
 
   for (let r = 0; r < boardSize; r++) {
     for (let c = 0; c < boardSize; c++) {
